a2a7a350178e64e5e5612ed2189a240d5193de8d
Elegant <curtisconley34@gmail.com>
2024-03-04 02:27:05 -0800

    vercel

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000; // You can use any port that's available
 
+app.use(express.static(path.join(__dirname, 'public')));
 
 const axios = require('axios');
 
